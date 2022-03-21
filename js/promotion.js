995254ed28b11ff8448af3a59241900d35329aaa
PashkaSharl <82102965+PashkaSharl@users.noreply.github.com>
2022-03-21 19:53:24 +0300

    new layout
    added a new site layout instead of the old one

---
+++ js/promotion.js
@@ -0,0 +1,11 @@
+const burger = document.querySelector('.header_menu') 
+const sidebar = document.querySelector('.sidebar')
+burger.addEventListener('click',()=>{ 
+    if(burger.classList.contains('active')){ 
+        burger.classList.remove('active')
+        sidebar.classList.remove('active')
+    }else{ 
+        burger.classList.add('active') 
+        sidebar.classList.add('active')
+    } 
+})
